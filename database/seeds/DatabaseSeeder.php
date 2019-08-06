1318700e4e545c5040c5c45a91f28ba234ff3997
openupmta <openupmta99@gmail.com>
2019-08-06 15:15:05 +0700

    tien

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use \Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call($this->role());
+    }
+    private function role()
+    {
+//        $location = "Cộng tác viên, Quản trị viên, Người dùng";
+//        $explode = explode(',',$location);
+        $location =array('Quản trị viên','Cộng tác viên','Người dùng');
+        for ($i = 0; $i <= 2; $i++ )
+        {
+            DB::table('role')->insert([
+                'id' => $i+1,
+                'name' => $location[$i],
+            ]);
+        }
+
     }
 }
