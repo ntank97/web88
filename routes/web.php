4012e8f184971b2d04d51e87fdc09c3c7b17dc62
thaoabc <thao19011999@gmail.com>
2019-08-26 09:02:35 +0700

    thao

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,11 @@ Route::get('kho-giao-dien','FrontendController@khoGiaoDien')->name('kho.giao.die
 Route::get('kho-giao-dien/{slug}-{id}','FrontendController@getListProduct')->name('get.list.product');
 Route::get('dich-vu/{slug}','FrontendController@getListService')->name('get.list.service');
 Route::get('lien-he','FrontendController@lienHe')->name('lien.he');
-<<<<<<< HEAD
-Route::get('lien-he/{slug}-{id}','FrontendController@getListProduct');
-Route::get('khach-hang','FrontendController@khachHang')->name('khach.hang');
-Route::get('other/{slug}', 'FrontendController@getListOtherService')->name('get.list.other.service');
-Route::get('news/{slug}','FrontendController@getListNews')->name('get.list.news');
-=======
 //Route::get('lien-he/{slug}-{id}','FrontendController@getListProduct');
 Route::get('khach-hang','FrontendController@khachHang')->name('khach.hang');
 Route::get('service/{slug}', 'FrontendController@getListOtherService')->name('get.list.other.service');
 Route::get('tin-tuc', 'FrontendController@tinTuc')->name('tin.tuc');
 Route::get('tin-tuc/{slug}','FrontendController@getListNews')->name('get.list.news');
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
 
 Route::get('seo','FrontendController@seo')->name('seo');
 Route::get('thiet-ke-website','FrontendController@thietKeWebsite')->name('thiet.ke.website');
@@ -46,11 +39,7 @@ Route::get('thiet-ke-web-chuan-seo-chuyen-nghiep', 'FrontendController@thietKeWe
 Route::get('thiet-ke-website', 'FrontendController@thietKeWebsite')->name('thiet.ke.website');
 Route::get('thiet-ke-website-theo-mau', 'FrontendController@thietKeWebsiteTheoMau')->name('thiet.ke.website.theo.mau');
 Route::get('thiet-ke-web-tron-goi-gia-re', 'FrontendController@thietKeWebTronGoiGiaRe')->name('thiet.web.tron.goi.gia.re');
-<<<<<<< HEAD
 Route::get('tin-tuc', 'FrontendController@tinTuc')->name('tin.tuc');
-=======
-
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
 Route::get('tuyen-dung', 'FrontendController@tuyenDung')->name('tuyen.dung');
 Route::get('thiet-ke-web-theo-yeu-cau','FrontendController@thietKeWebTheoYeuCau')->name('thiet.ke.web.theo.yeu.cau');
 Route::get('dich-vu-thiet-ke-web-gia-re','FrontendController@dichVuThietKeWebGiaRe')->name('dich.vu.thiet.ke.web.gia.re');
@@ -81,13 +70,10 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
             Route::get('edit/{id}','AccountController@edit')->name('editor.account.edit');
             Route::post('update/{id}','AccountController@update')->name('editor.account.update');
             Route::get('delete/{id}','AccountController@delete')->name('editor.account.delete');
-<<<<<<< HEAD
 
             Route::post('update_pending','WebStoreController@update_pending')->name('update_pending');
             Route::post('update_pending_blogs','WebStoreController@update_pending_blogs')->name('update_pending_blogs');
             Route::post('update_pending_services','WebStoreController@update_pending_services')->name('update_pending_services');
-=======
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
         });
         Route::group(['prefix' => 'user'],function(){
             Route::get('profile','ClientController@profile')->name('editor.account.index');
@@ -116,7 +102,6 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
         Route::get('delete/{id}','PartnerController@delete')->name('partner.delete');
     });
 
-<<<<<<< HEAD
     Route::prefix('contact')->group(function(){
         Route::get('list','ContactController@list')->name('contact.list');
         Route::get('add','ContactController@add')->name('contact.add');
@@ -134,15 +119,6 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
         Route::post('edit/{id}','SupportsController@update')->name('supports.edit');
         Route::get('delete/{id}','SupportsController@delete')->name('supports.delete');
     });
-
-    Route::prefix('blogs')->group(function(){
-        Route::get('list','BlogsController@list')->name('blogs.list');
-        Route::get('add','BlogsController@create')->name('blogs.create');
-        Route::post('add','BlogsController@store')->name('blogs.add');
-        Route::get('edit/{id}','BlogsController@edit')->name('blogs.edit');
-        Route::post('edit/{id}','BlogsController@update')->name('blogs.edit');
-        Route::get('delete/{id}','BlogsController@delete')->name('blogs.delete');
-=======
     Route::prefix('blogs')->group(function () {
         Route::get('/list', 'BlogsController@index')->name('blogs.index');
         Route::get('/add', 'BlogsController@create')->name('blogs.create');
@@ -157,7 +133,6 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
         Route::get('/detail/{id}', 'BlogsController@detail')->name('blogs.detail');
         Route::get('/setactive/{id}/{status}', 'BlogsController@setactive')->name('blogs.setactive');
         Route::get('/setactive-cate/{id}/{status}', 'BlogsController@setactiveCate')->name('blogs.setactiveCate');
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
     });
     //    News
     Route::prefix('web-store')->group(function () {
@@ -168,11 +143,6 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
         Route::post('/add-cate', 'WebStoreController@storeCate')->name('webstore.storeCate');
         Route::get('/edit/{id}', 'WebStoreController@edit')->name('webstore.edit');
         Route::post('/edit/{id}', 'WebStoreController@update')->name('webstore.update');
-<<<<<<< HEAD
-=======
-        Route::get('/edit-cate/{id}', 'WebStoreController@editCate')->name('webstore.editCate');
-        Route::post('/edit-cate/{id}', 'WebStoreController@updateCate')->name('webstore.updateCate');
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
         Route::get('/destroy/{id}', 'WebStoreController@destroy')->name('webstore.destroy');
         Route::get('/destroy-cate/{id}', 'WebStoreController@destroyCate')->name('webstore.destroyCate');
         Route::get('/show/{id}', 'WebStoreController@show')->name('webstore.show');
@@ -185,13 +155,8 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
         Route::get('/list', 'ServiceController@index')->name('service.index');
         Route::get('/add', 'ServiceController@create')->name('service.create');
         Route::post('/add', 'ServiceController@store')->name('service.store');
-<<<<<<< HEAD
         Route::get('/add-cate', 'ServiceController@createCate')->name('service.createCate');
         Route::post('/add-cate', 'ServiceController@storeCate')->name('service.storeCate');
-=======
-//        Route::get('/add-cate', 'ServiceController@createCate')->name('service.createCate');
-//        Route::post('/add-cate', 'ServiceController@storeCate')->name('service.storeCate');
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
         Route::get('/edit/{id}', 'ServiceController@edit')->name('service.edit');
         Route::post('/edit/{id}', 'ServiceController@update')->name('service.update');
         Route::get('/destroy/{id}', 'ServiceController@destroy')->name('service.destroy');
@@ -203,21 +168,6 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
     });
 //    Other Service
     Route::prefix('other-service')->group(function () {
-<<<<<<< HEAD
-        Route::get('/list', 'OtherServiceController@index')->name('otherservice.index');
-        Route::get('/add', 'OtherServiceController@create')->name('otherservice.create');
-        Route::post('/add', 'OtherServiceController@store')->name('otherservice.store');
-        Route::get('/add-cate', 'OtherServiceController@createCate')->name('otherservice.createCate');
-        Route::post('/add-cate', 'OtherServiceController@storeCate')->name('otherservice.storeCate');
-        Route::get('/edit/{id}', 'OtherServiceController@edit')->name('otherservice.edit');
-        Route::post('/edit/{id}', 'OtherServiceController@update')->name('otherservice.update');
-        Route::get('/destroy/{id}', 'OtherServiceController@destroy')->name('otherservice.destroy');
-        Route::get('/destroy-cate/{id}', 'OtherServiceController@destroyCate')->name('otherservice.destroyCate');
-        Route::get('/show/{id}', 'OtherServiceController@show')->name('otherservice.show');
-        Route::get('/detail/{id}', 'OtherServiceController@detail')->name('otherservice.detail');
-        Route::get('/setactive/{id}/{status}', 'OtherServiceController@setactive')->name('otherservice.setactive');
-        Route::get('/setactive-cate/{id}/{status}', 'OtherServiceController@setactiveCate')->name('otherservice.setactiveCate');
-=======
         Route::get('/list', 'OtherServiceController@index')->name('other_service.index');
         Route::get('/add', 'OtherServiceController@create')->name('other_service.create');
         Route::post('/add', 'OtherServiceController@store')->name('other_service.store');
@@ -255,7 +205,6 @@ Route::group(['prefix' => 'admin','middleware' => 'CheckAdmin'],function(){
         Route::get('/show/{id}', 'SliderContentController@show')->name('slidercontent.show');
         Route::get('/detail/{id}', 'SliderContentController@detail')->name('slidercontent.detail');
         Route::get('/setactive/{id}/{status}', 'SliderContentController@setactive')->name('slidercontent.setactive');
->>>>>>> 2617931d226679f7de185ff4b00843c67d7b875b
     });
 });
 
